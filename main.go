67b3b80ef6df7b7ce5bf4a00ed1cb2ae0a8f525a
Kevin Blues <kbluescode@gmail.com>
2017-02-05 15:54:51 -0800

    finished full functionality of connect-four game

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -11,13 +12,16 @@ import (
 
 const (
 	gridSize    int  = 5
-	turnMax     int  = 5
+	turnMax     int  = 10
 	defaultMark rune = ' '
+	p1Mark      rune = 'x'
+	p2Mark      rune = 'o'
 )
 
 var (
-	grid    [gridSize][gridSize]*Cell
+	grid    Grid
 	scanner = bufio.NewScanner(os.Stdin)
+	turn    int
 )
 
 // Cell - A single cell in the grid
@@ -30,23 +34,41 @@ func (c *Cell) String() string {
 	return fmt.Sprintf("[%c]", c.val)
 }
 
+// Grid - a grid of cells
+type Grid [gridSize][gridSize]*Cell
+
+// Cell - retrieves a cell from grid or returns an error
+func (g *Grid) Cell(row, col int) (cell *Cell, err error) {
+	if (row < 0 || row >= gridSize) || (col < 0 || col >= gridSize) {
+		err = fmt.Errorf("Invalid row or column given, must be between 0 and %v", gridSize)
+	}
+	if err == nil {
+		cell = grid[row][col]
+	}
+	return cell, err
+}
+
 func main() {
 	resetGrid()
 	gameLoop()
 }
 
 func gameLoop() {
-	currentMark := 'x'
-	for turn := 0; turn < turnMax; turn++ {
+	currentMark := p1Mark
+	var won bool
+	var mark rune
+	for ; !won; won, mark = hasWon() {
 		fmt.Printf("Current player: '%c'\n\n", currentMark)
 		displayGrid()
 		takeTurn(currentMark)
-		if currentMark == 'x' {
-			currentMark = 'o'
+		if currentMark == p1Mark {
+			currentMark = p2Mark
 		} else {
-			currentMark = 'x'
+			currentMark = p1Mark
 		}
 	}
+	fmt.Printf("\n\nGame Over:\nPlayer %q has won!\n", mark)
+	displayGrid()
 }
 
 func resetGrid() {
@@ -70,22 +92,133 @@ func displayGrid() {
 }
 
 func takeTurn(mark rune) {
-	if scanner.Scan() {
-		text := scanner.Text()
-		if strings.ContainsRune(text, ' ') {
-			chunks := strings.Split(text, " ")
-			if len(chunks) > 1 {
-				row, err1 := strconv.Atoi(chunks[0])
-				col, err2 := strconv.Atoi(chunks[1])
-				if err1 == nil && err2 == nil &&
-					row >= 0 && row < gridSize &&
-					col >= 0 && col < gridSize {
-					cell := grid[row][col]
-					if cell.val == defaultMark {
-						cell.val = mark
-					}
+	for {
+		if scanner.Scan() {
+			text := scanner.Text()
+			if commandChunks, err := parseCommand(text); err == nil {
+				cell, err := grid.Cell(commandChunks[0], commandChunks[1])
+				if err != nil {
+					fmt.Println(err)
+				} else if cell.val != defaultMark {
+					fmt.Println("Invalid move: choose a space that hasn't been chosen")
+				} else {
+					cell.val = mark
+					break
 				}
 			}
 		}
 	}
+}
+
+func parseCommand(command string) ([]int, error) {
+	var chunks []int
+
+	if !strings.ContainsRune(command, ' ') {
+		return chunks, errors.New("Command must contain a ' ' between row and column")
+	}
+
+	stringChunks := strings.Split(command, " ")
+	for _, val := range stringChunks {
+		num, err := strconv.Atoi(val)
+		if err != nil {
+			return chunks, err
+		}
+		chunks = append(chunks, num)
+	}
+
+	return chunks, nil
+}
+
+// hasWon returns true if any line of 4 of 'x' or 'o' has been made in the grid
+func hasWon() (bool, rune) {
+	if fourConnected(p1Mark) {
+		return true, p1Mark
+	} else if fourConnected(p2Mark) {
+		return true, p2Mark
+	}
+
+	return false, defaultMark
+}
+
+// fourConnected returns true if collection of cells has a contiguous line of 4 or false if length of cells is < 4 or no contiguous line is present
+func fourConnected(symbol rune) bool {
+	// check horizontal
+	for row := 0; row < gridSize; row++ {
+		contiguousCount := 0
+		for col := 0; col < gridSize && contiguousCount < 4; col++ {
+			cell := grid[row][col]
+			if cell.val == symbol {
+				contiguousCount++
+			} else {
+				contiguousCount = 0
+			}
+		}
+		if contiguousCount == 4 {
+			return true
+		}
+	}
+
+	// check vertical
+	for col := 0; col < gridSize; col++ {
+		contiguousCount := 0
+		for row := 0; row < gridSize && contiguousCount < 4; row++ {
+			cell := grid[row][col]
+			if cell.val == symbol {
+				contiguousCount++
+			} else {
+				contiguousCount = 0
+			}
+		}
+		if contiguousCount == 4 {
+			return true
+		}
+	}
+
+	//check down-right angles
+	startingCells := make([]*Cell, 0)
+	for col := 0; col < gridSize; col++ {
+		startingCells = append(startingCells, grid[0][col])
+	}
+	for row := 0; row < gridSize; row++ {
+		startingCells = append(startingCells, grid[row][0])
+	}
+	for _, start := range startingCells {
+		contiguousCount := 0
+		for row, col := start.row, start.col; row < gridSize && col < gridSize && contiguousCount < 4; row, col = row+1, col+1 {
+			cell := grid[row][col]
+			if cell.val == symbol {
+				contiguousCount++
+			} else {
+				contiguousCount = 0
+			}
+		}
+		if contiguousCount == 4 {
+			return true
+		}
+	}
+
+	//check down-left angles
+	startingCells = make([]*Cell, 0)
+	for col := gridSize - 1; col <= 0; col-- {
+		startingCells = append(startingCells, grid[gridSize-1][col])
+	}
+	for row := gridSize - 1; row <= 0; row-- {
+		startingCells = append(startingCells, grid[row][gridSize-1])
+	}
+	for _, start := range startingCells {
+		contiguousCount := 0
+		for row, col := start.row, start.col; row >= 0 && col >= 0 && contiguousCount < 4; row, col = row-1, col-1 {
+			cell := grid[row][col]
+			if cell.val == symbol {
+				contiguousCount++
+			} else {
+				contiguousCount = 0
+			}
+		}
+		if contiguousCount == 4 {
+			return true
+		}
+	}
+
+	return false
 }
