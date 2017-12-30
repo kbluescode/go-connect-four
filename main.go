651d85ce56683e6ef426e47b971425d9b0d71e7d
Kevin Blues <kbluescode@gmail.com>
2017-12-30 10:30:25 -0800

    refactored/fixed diagonal checks, improved printing of grids/cells, removed extra pointers on methods

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ type Cell struct {
 }
 
 // String - to satisfy the Stringer interface for fmt
-func (c *Cell) String() string {
+func (c Cell) String() string {
 	return fmt.Sprintf("[%c]", c.val)
 }
 
@@ -39,7 +39,7 @@ func (c *Cell) String() string {
 type Grid [gridSize][gridSize]*Cell
 
 // Cell - retrieves a cell from grid or returns an error
-func (g *Grid) Cell(row, col int) (cell *Cell, err error) {
+func (g Grid) Cell(row, col int) (cell *Cell, err error) {
 	if (row < 0 || row >= gridSize) || (col < 0 || col >= gridSize) {
 		err = fmt.Errorf("Invalid row or column given, must be between 0 and %v", gridSize)
 	}
@@ -47,6 +47,19 @@ func (g *Grid) Cell(row, col int) (cell *Cell, err error) {
 		cell = grid[row][col]
 	}
 	return cell, err
+}
+
+// String - implements stringer interface for Grid
+func (g Grid) String() string {
+	buffer := bytes.NewBufferString("   0  1  2  3  4\n")
+	for row := 0; row < gridSize; row++ {
+		buffer.WriteString(fmt.Sprintf("%d ", row))
+		for col := 0; col < gridSize; col++ {
+			buffer.WriteString(grid[row][col].String())
+		}
+		buffer.WriteRune('\n')
+	}
+	return buffer.String()
 }
 
 func main() {
@@ -61,7 +74,7 @@ func gameLoop() {
 	currentMark := p1Mark
 	for ; !won && turn <= maxTurn; won, winnerMark = hasWon() {
 		fmt.Printf("Current player: '%c'\n\n", currentMark)
-		displayGrid()
+		fmt.Println(grid)
 		takeTurn(currentMark)
 		if currentMark == p1Mark {
 			currentMark = p2Mark
@@ -75,33 +88,26 @@ func gameLoop() {
 	} else {
 		fmt.Printf("\n\nGame Over:\nPlayer %q has won!\n", winnerMark)
 	}
-	displayGrid()
+	fmt.Println(grid)
 }
 
 // resetGrid - reverts the grid to its base state of blank cells
 func resetGrid() {
 	for row := 0; row < gridSize; row++ {
 		for col := 0; col < gridSize; col++ {
-			grid[row][col] = &Cell{row: row, col: col, val: defaultMark}
+			cell := grid[row][col]
+			if cell == nil {
+				grid[row][col] = &Cell{row: row, col: col, val: defaultMark}
+			} else {
+				cell.val = defaultMark
+			}
 		}
 	}
-}
-
-// displayGrid - prints out the grid in a human-readable format
-func displayGrid() {
-	buffer := bytes.NewBufferString("   0  1  2  3  4\n")
-	for row := 0; row < gridSize; row++ {
-		buffer.WriteString(fmt.Sprintf("%d ", row))
-		for col := 0; col < gridSize; col++ {
-			buffer.WriteString(grid[row][col].String())
-		}
-		buffer.WriteRune('\n')
-	}
-	fmt.Println(buffer.String())
 }
 
 // takeTurn - will attempt to get input from the user and display errors if the input is invalid
 func takeTurn(mark rune) {
+	fmt.Println("[row],[col]")
 	for {
 		if scanner.Scan() {
 			text := scanner.Text()
@@ -125,11 +131,11 @@ func takeTurn(mark rune) {
 func parseCommand(command string) ([]int, error) {
 	var chunks []int
 
-	if !strings.ContainsRune(command, ' ') {
-		return chunks, errors.New("Command must contain a ' ' between row and column")
+	if !strings.ContainsRune(command, ',') {
+		return chunks, errors.New("Command must contain a ',' between row and column")
 	}
 
-	stringChunks := strings.Split(command, " ")
+	stringChunks := strings.Split(command, ",")
 	for _, val := range stringChunks {
 		num, err := strconv.Atoi(val)
 		if err != nil {
@@ -158,12 +164,7 @@ func fourConnected(symbol rune) bool {
 	for row := 0; row < gridSize; row++ {
 		contiguousCount := 0
 		for col := 0; col < gridSize && contiguousCount < 4; col++ {
-			cell := grid[row][col]
-			if cell.val == symbol {
-				contiguousCount++
-			} else {
-				contiguousCount = 0
-			}
+			contiguousCount = handleMatch(contiguousCount, grid[row][col].val, symbol)
 		}
 		if contiguousCount == 4 {
 			return true
@@ -174,63 +175,43 @@ func fourConnected(symbol rune) bool {
 	for col := 0; col < gridSize; col++ {
 		contiguousCount := 0
 		for row := 0; row < gridSize && contiguousCount < 4; row++ {
-			cell := grid[row][col]
-			if cell.val == symbol {
-				contiguousCount++
-			} else {
-				contiguousCount = 0
-			}
+			contiguousCount = handleMatch(contiguousCount, grid[row][col].val, symbol)
 		}
 		if contiguousCount == 4 {
 			return true
 		}
 	}
 
-	//check down-right angles
-	startingCells := make([]*Cell, 0)
-	for col := 0; col < gridSize; col++ {
-		startingCells = append(startingCells, grid[0][col])
-	}
-	for row := 0; row < gridSize; row++ {
-		startingCells = append(startingCells, grid[row][0])
-	}
-	for _, start := range startingCells {
-		contiguousCount := 0
-		for row, col := start.row, start.col; row < gridSize && col < gridSize && contiguousCount < 4; row, col = row+1, col+1 {
-			cell := grid[row][col]
-			if cell.val == symbol {
-				contiguousCount++
-			} else {
-				contiguousCount = 0
-			}
-		}
+	// check down-right angle
+	// 0,0 1,1 2,2 3,3 4,4, etc.
+	contiguousCount := 0
+	for col, row := 0, 0; col < gridSize && contiguousCount < 4; col, row = col+1, row+1 {
+		contiguousCount = handleMatch(contiguousCount, grid[row][col].val, symbol)
 		if contiguousCount == 4 {
 			return true
 		}
 	}
 
 	//check down-left angles
-	startingCells = make([]*Cell, 0)
-	for col := gridSize - 1; col <= 0; col-- {
-		startingCells = append(startingCells, grid[gridSize-1][col])
-	}
-	for row := gridSize - 1; row <= 0; row-- {
-		startingCells = append(startingCells, grid[row][gridSize-1])
-	}
-	for _, start := range startingCells {
-		contiguousCount := 0
-		for row, col := start.row, start.col; row >= 0 && col >= 0 && contiguousCount < 4; row, col = row-1, col-1 {
-			cell := grid[row][col]
-			if cell.val == symbol {
-				contiguousCount++
-			} else {
-				contiguousCount = 0
-			}
-		}
+	// 0,4 1,3 2,2 3,1 4,0, etc.
+	contiguousCount = 0
+	for col, row := 4, 0; col >= 0 && contiguousCount < 4; col, row = col-1, row+1 {
+		contiguousCount = handleMatch(contiguousCount, grid[row][col].val, symbol)
 		if contiguousCount == 4 {
 			return true
 		}
 	}
 
 	return false
+}
+
+// handleMatch - takes current contiguous count, cell value and active player's symbol
+//	-> returning the updated contiguous count
+func handleMatch(currentCount int, cellVal, symbol rune) int {
+	if cellVal == symbol {
+		currentCount++
+	} else {
+		currentCount = 0
+	}
+	return currentCount
 }
